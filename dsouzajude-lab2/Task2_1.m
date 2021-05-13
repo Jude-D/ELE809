3efe7a3ac49749e43c59d628d1d06dbaeb6c909b
Jude D'Souza <57837350+Jude-D@users.noreply.github.com>
2021-05-12 21:30:55 -0400

    Upload for lab 2

---
+++ dsouzajude-lab2/Task2_1.m
@@ -0,0 +1,40 @@
+Tf = 10.0;
+Fs = 150.0;
+Ts = 1.0/Fs;
+Kp = 44.4;
+Ki = 0.03;
+Kd = 0.06;
+Beta = Kd/Kp/20;
+
+t  = 0:Ts:Tf;
+r = 50*pi/180*square(2*pi*0.25*t);
+Ns = length(t);
+
+num = [4.225 -3.226];
+den = [1 -1.765 0.765];
+Gp = tf(num, den);
+Gc = pid(Kp, Ki, Kd);
+H = [1];
+M = feedback(Gc*Gp, H);
+
+theta = [];
+radfac = 180.0/pi;
+
+servo = Initialize(Fs, 1, Tf); 
+
+for k = 1: Ns,
+    motor_acc = ReadEncoder(servo);
+    
+    motor_rad = EtoR(motor_acc);
+    
+    Va = M*r(k);
+    
+    DtoA(servo, VtoD(Va));
+    
+    theta(k) = motor_rad;
+    
+end;
+
+Terminate(servo);
+
+plot(t,radfac*r, t, radfac*theta);
